9ae069779ff2790972df8efbea7e247df11756a2
Adriana Cruz <cruz.drikazurc@gmail.com>
2024-04-01 20:53:09 -0300

    Resolve questão 3

--- entrada_e_saida.py
+++ entrada_e_saida.py
@@ -10,4 +10,12 @@ def somar():
     print ("A soma dos numeros é:", somar)
 somar()
 
+def multiplicar():
+    primeiro_numero = int(input ("Digite primeiro numero: "))
+    segundo_numero = int(input ("Digite segundo numero: "))
+    multiplicar = primeiro_numero * segundo_numero
+    print ("O produto dos numeros é:", multiplicar)
+multiplicar()
+
+
     
