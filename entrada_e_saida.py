7c442c1c39852faaaf42ad167c8dc3ff37a87e8c
Adriana Cruz <cruz.drikazurc@gmail.com>
2024-04-01 20:43:01 -0300

    Resolve questão 2

--- entrada_e_saida.py
+++ entrada_e_saida.py
@@ -2,3 +2,12 @@ def saudar():
     nome = input("Qual seu nome?")
     print("Olá, ", nome,"! Seja bem-vindo(a)!")
 saudar()
+
+def somar():
+    primeiro_numero = int(input ("Digite primeiro numero: "))
+    segundo_numero = int(input ("Digite segundo numero: "))
+    somar = primeiro_numero + segundo_numero
+    print ("A soma dos numeros é:", somar)
+somar()
+
+    
