0ca10d39a588360f8c7b4d5931ee7d870a32ca8f
Adriana Cruz <cruz.drikazurc@gmail.com>
2024-04-01 21:19:51 -0300

    Resolve questão 6

---
+++ condicionais.py
@@ -0,0 +1,9 @@
+def verificar_idade():
+    idade = int(input("Qual sua idade?"))
+    if(idade >=18):
+        print("É maior de idade")
+    else:
+        print("É menor de  idade")
+verificar_idade()
+
+        
