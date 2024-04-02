92baed6c4996bb2577caa55f691f150eff760341
Adriana Cruz <cruz.drikazurc@gmail.com>
2024-04-01 21:27:41 -0300

    Resolve questão 7

--- condicionais.py
+++ condicionais.py
@@ -6,4 +6,13 @@ def verificar_idade():
         print("É menor de  idade")
 verificar_idade()
 
-        
+def verificar_par_impar():
+    numero_digitado = int(input("Digite um número inteiro: "))
+    if numero_digitado % 2 == 0:
+        print("O número é par.") 
+    else:
+        print("O número é ímpar.")
+
+verificar_par_impar()
+
+
