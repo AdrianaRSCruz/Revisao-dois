7da61ef4b5d9d435c63f75e75bb67c77b7a30378
Adriana Cruz <cruz.drikazurc@gmail.com>
2024-04-03 18:22:49 -0300

    Verifica idade

--- condicionais.py
+++ condicionais.py
@@ -6,26 +6,3 @@ def verificar_idade():
         print("É menor de  idade")
 verificar_idade()
 
-def verificar_par_impar():
-    numero_digitado = int(input("Digite um número inteiro: "))
-    if numero_digitado % 2 == 0:
-        print("O número é par.") 
-    else:
-        print("O número é ímpar.")
-
-verificar_par_impar()
-
-def verificar_aprovacao():
-    nota_aluno = float(input("Digite a nota do aluno: "))
-    if(nota_aluno >= 7):
-        print("Aprovado")
-    elif (nota_aluno <= 6):
-        print("Reprovado")
-        
-    else:
-        print("Em recuperação")
-
-verificar_aprovacao()
-
-
-
